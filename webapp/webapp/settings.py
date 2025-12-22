27d96bee040fb0551794be5a4c5ddcc2c5c41476
Rnx107 <neupaneraman12@gmail.com>
2025-12-22 10:57:53 +0545

    Added apps

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'core',
     'rest_framework',
     'corsheaders',
+    'accounts',
+    'students', 
+    'teachers',
+    'courses',
+    'attendance',
+    'dashboard',
+    'reports',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +74,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
